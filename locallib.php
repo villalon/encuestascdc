563e491947922a7d8e469265a99299619e0d0a95
Ubuntu <ubuntu@onlinedev.uai.cl>
2020-08-03 15:39:22 -0400

    intento 2

--- locallib.php
+++ locallib.php
@@ -1557,7 +1557,8 @@ function encuestascdc_obtiene_estadisticas_por_seccion_global($stats) {
     foreach($stats as $courseid => $statcourse) {
         foreach($statcourse as $seccion => $statstype) {
             foreach($statstype as $type => $statdetail) {
-                $nombre_encuestapregunta = [$detail['respuesta']->nombre];
+                $nombre_encuestapregunta = strval([$detail['respuesta']->nombre][0]);
+                var_dump($nombre_encuestapregunta);
                 //strpos($mystring, $word)
 
                 if($type === 'Rate (scale 1..5)') {
